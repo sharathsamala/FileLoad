975f5a5d4777c240e5b8c985071e12719655fedb
Sharath Samala <sharathsamala@live.com>
2017-01-04 17:01:14 +0530

    Create configLoader.py

---
+++ util/configLoader.py
@@ -0,0 +1,43 @@
+#***********************************************************************************************
+#* Loads the configuration from .cfg file in current directory and returns the params dictionary
+#*
+#* Author: Kapil Sharma (ks515n)
+#* Date: 08/30/2016
+#***********************************************************************************************
+def getConfig(configPath, logger):
+    params = {} 
+    logger.info('Parsing configuration from ' + configPath)
+    
+    with open (configPath, 'r') as cfg:
+        for line in cfg:
+            line = line.rstrip() #removes trailing whitespace and '\n' chars
+            
+            if '=' not in line: 
+                continue #skips blanks and comments w/o =
+            
+            if line.startswith('#'): 
+                continue #skips comments which contain =
+            
+            key, value = line.split('=', 1)
+            
+            if key == "emailFrom":
+                if '@' not in value:
+                    params[key] = value.strip() + '@att.com' 
+                else:
+                    params[key] = value.strip()            
+            elif key == 'emailTo':
+                emailTo = []
+            
+                for email in value.strip().split(','):
+                    if '@' not in email:
+                        emailTo.append(email.strip() + '@att.com')
+                    else:
+                        emailTo.append(email.strip())
+                        
+                params[key] = emailTo
+            else:
+                params[key] = value.strip()
+                
+    logger.info('Configuration loaded')
+    
+    return params
